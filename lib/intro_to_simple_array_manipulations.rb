27d98860f528489d965e9a6d2e44af55c96a7483
Eliza Munroe <era3munroe@gmail.com>
2019-06-30 14:28:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,7 +23,7 @@ def shift_with_args(array)
 end
 
 def using_concat(array)
-  @all_my_favs = @my_favorite_things.concat([more_favs])
+  array.concat([my_favorite_things] [more_favs])
 end
 
 def using_insert (array, element)
